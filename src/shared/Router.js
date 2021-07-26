271e2559fde2ddf9851ef2d433546f24f745de9c
with-key <rem9238@kakao.com>
2021-07-27 02:03:39 +0900

    feat : Login routing url 설정

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 
 // pages
 import Intro from "../pages/Intro";
+import Login from "../pages/Login";
 import Auth from "../shared/auth";
 import Register from "../pages/Register";
 
@@ -10,6 +11,7 @@ const Router = () => {
   return (
     <Switch>
       <Route path="/" component={Auth(Intro, null)} exact />
+      <Route path="/login" component={Auth(Login, null)} exact />
       <Route path="/register" component={Auth(Register, null)} exact />
     </Switch>
   );

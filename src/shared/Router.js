b5af890dde17e6e7fe47642372b5efa743f1d340
with-key <rem9238@kakao.com>
2021-07-27 01:59:41 +0900

    feat : Register routing url 설정

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Intro from '../pages/Intro';
-import Login from '../pages/Login';
-import Auth from '../shared/auth';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+// pages
+import Intro from "../pages/Intro";
+import Auth from "../shared/auth";
+import Register from "../pages/Register";
 
 const Router = () => {
-	return (
-		<Switch>
-			<Route path='/' component={Auth(Intro, null)} exact />
-			<Route path='/login' component={Auth(Login, null)} exact />
-			<Route path='/login' component={Auth(Login, null)} exact />
-		</Switch>
-	);
+  return (
+    <Switch>
+      <Route path="/" component={Auth(Intro, null)} exact />
+      <Route path="/register" component={Auth(Register, null)} exact />
+    </Switch>
+  );
 };
 
 export default Router;

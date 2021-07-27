7a2ada660ea5b316f7308d205fb3f1a453044f72
slamdunk11 <61729276+slamdunk11@users.noreply.github.com>
2021-07-28 02:59:54 +0900

    edit : roomApi api 수정

--- src/api/roomApi.js
+++ src/api/roomApi.js
@@ -2,7 +2,7 @@ import RoomList from "../pages/RoomList";
 import { instance } from "./index";
 
 export const roomApi = {
-  getRoomList: (roomList) => instance.get("api/rooms", roomList),
-  getOneRoom: (roomId) => instance.get("api/rooms/:roomId/main", roomId),
-  addRoom: (room) => instance.post("api/room", room),
+  getRoomList: (roomList) => instance.get("/rooms", roomList),
+  getOneRoom: (roomId) => instance.get(`/rooms/${roomId}/main`, roomId),
+  addRoom: (room) => instance.post("/room", room),
 };

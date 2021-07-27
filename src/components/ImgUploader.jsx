ac3df3768be075d66b52960ca29f4ce6dd8960f9
slamdunk11 <61729276+slamdunk11@users.noreply.github.com>
2021-07-28 03:21:41 +0900

    edit : ImgUploader 일부 AddRoomModal으로 이동하여 코드 수정

--- src/components/ImgUploader.jsx
+++ src/components/ImgUploader.jsx
@@ -1,27 +1,20 @@
 import AWS from "aws-sdk";
 import React from "react";
 import styled from "styled-components";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 
 //element
 import Input from "../elem/Input";
 
 //redux
-import image from "../redux/modules/image";
+import {setPreview} from "../redux/modules/image";
 
-const ImgUploader = (props) => {
+const ImgUploader = ({fileInput}) => {
 
-  AWS.config.update({
-    region: "ap-northeast-2",
-    credentials: new AWS.CognitoIdentityCredentials({
-      IdentityPoolId: "ap-northeast-2:a2a935f7-cd6a-47b9-8e6b-c02b7b2e0bb2", // cognito 인증 풀에서 받아온 키를 문자열로 입력합니다. (Ex. "ap-northeast-2...")
-    }),
-  });
-  
   const dispatch = useDispatch();
-  const fileInput = React.useRef();
+  const preview = useSelector((state) => state.image.preview);
 
-  const selectFile = (e) => {
+  const imagePreview = (e) => {
 
     const reader = new FileReader();
     const file = fileInput.current.files[0];
@@ -29,32 +22,18 @@ const ImgUploader = (props) => {
     reader.readAsDataURL(file);
 
     reader.onloadend = () => {
-      dispatch(image.setPreview(reader.result));
+      dispatch(setPreview(reader.result));
     }
   }
-
-  const handleFileInput = (e) => {
-    const file = e.target.files[0];
-
-    const upload = new AWS.S3.ManagedUpload({
-      params: {
-        Bucket: "teampigbucket",
-        Key: file.name,
-        Body: file,
-      },
-    });
-
-    const promise = upload.promise();
-
-    promise.then(
-      function (data) {
-        alert("이미지 업로드에 성공했습니다.");
-      },
-      function (err) {
-        return alert("오류가 발생했습니다:", err.message);
-      }
-    );
-  };
+  
+  AWS.config.update({
+    region: "ap-northeast-2",
+    credentials: new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: "ap-northeast-2:a2a935f7-cd6a-47b9-8e6b-c02b7b2e0bb2", // cognito 인증 풀에서 받아온 키를 문자열로 입력합니다. (Ex. "ap-northeast-2...")
+    }),
+  });
+  
+  
 
   return (
     <>
@@ -62,10 +41,10 @@ const ImgUploader = (props) => {
         type="file"
         id="upload"
         className="image-upload"
-        onChange={handleFileInput}
+        onChange={imagePreview}
         ref={fileInput}
       />
-      <label htmlFor="upload" className="image-upload-wrapper"></label>
+      <label htmlFor="upload" preview={preview} className="image-upload-wrapper"></label>
     </>
   );
 };

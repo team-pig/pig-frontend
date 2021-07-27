e37ffc2da5515c6a8016dc72e7c538bd5571b621
An Jihyun <hwiyu25@gmail.com>
2021-07-28 03:02:22 +0900

    feat : Page 생성 - Workspace 및 Router 적용

---
+++ src/pages/Workspace.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Workspace = (props) => {
+  return (
+    <>
+      {/* Room 별도 헤더가 있는 경우 */}
+      {/* <RoomHeader /> */}
+      <div>
+        <div>
+          {/* <RoomTabs /> */}
+          <section></section>
+        </div>
+        {/* <RoomSideBar /> */}
+      </div>
+    </>
+  );
+};
+
+export default Workspace;

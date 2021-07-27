6254810f8a27efc3168178b42690d6ea4c049ca0
with-key <rem9238@kakao.com>
2021-07-27 17:17:29 +0900

    feat : Footer component 추가

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Footer = () => {
+  return <Container></Container>;
+};
+
+const Container = styled.footer`
+  width: 100%;
+  height: 100px;
+  background-color: #eee;
+`;
+export default Footer;

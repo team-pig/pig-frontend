286dbcc8ea7739a4a98c4b738ba6bd03c5773c00
slamdunk11 <61729276+slamdunk11@users.noreply.github.com>
2021-07-27 23:01:15 +0900

    edit : 오타 확인 후 코드 수정

--- src/redux/modules/room.js
+++ src/redux/modules/room.js
@@ -1,13 +1,12 @@
 import { createAction, handleActions } from "redux-actions";
 import produce from "immer";
 import { roomApi } from "../api/roomApi";
-import { actionCreators } from "../../../../../../Downloads/src/redux/modules/image";
-import { DRAFT_STATE } from "immer/dist/internal";
 
 //action
 const ADD_ROOM = "room/ADD_ROOM";
 const GET_ROOM_LIST = "room/GET_ROOM_LIST";
-const GET_ROOM = "room/GET_ROOM";
+const GET_ONE_ROOM = "room/GET_ONE_ROOM";
+const LOADING = "LOADING";
 
 //initialState
 const initialState = {
@@ -18,9 +17,13 @@ const initialState = {
 
 //action creator
 export const addRoom = createAction(ADD_ROOM, (room) => ({ room }));
-// export const getRoomList = createAction(GET_ROOM_LIST, (roomList) => ({
-//   roomList,
-// }));
+export const getRoomList = createAction(GET_ROOM_LIST, (roomList) => ({
+  roomList,
+}));
+export const getOneRoom = createAction(GET_ONE_ROOM, (roomId) => ({
+  roomId,
+}));
+export const loading = createAction(LOADING, (is_loading) => ({ is_loading }));
 
 //thunk function
 export const __addRoom =
@@ -41,6 +44,24 @@ export const __addRoom =
     }
   };
 
+export const __getRoomList =
+  () =>
+  async (dispatch, getState, { history }) => {
+    try {
+    } catch (e) {}
+  };
+
+export const __getOneRoom =
+  (roomId) =>
+  async (dispatch, getState, { history }) => {
+    try {
+      const { data } = await roomApi.getOneRoom(roomId);
+      dispatch(getOneRoom(data));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
 //reducer
 const room = handleActions(
   {
@@ -52,8 +73,4 @@ const room = handleActions(
   initialState
 );
 
-const actionCreaators = {
-  addRoom,
-};
-
-export { actionCreators };
+export default room;

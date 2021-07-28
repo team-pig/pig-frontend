eceec46448bafc887c0e9eb63129be6c31b506c0
slamdunk11 <61729276+slamdunk11@users.noreply.github.com>
2021-07-29 02:40:26 +0900

    feat : roomApi editRoom 추가

--- src/api/roomApi.js
+++ src/api/roomApi.js
@@ -2,8 +2,9 @@ import RoomList from "../pages/RoomList";
 import { instance } from "./index";
 
 export const roomApi = {
-  getRoomList: (roomList) => instance.get("/rooms", roomList),
+  getRoomList: () => instance.get("/rooms"),
   getOneRoom: (roomId) => instance.get(`/rooms/${roomId}/main`, roomId),
   addRoom: (room) => instance.post("/room", room),
+  editRoom: (roomId, content) => instance.put("/room", { content }),
   deleteRoom: (roomId) => instance.delete("/room"),
 };

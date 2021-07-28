fd6229947759a8b9fe7a381a46e9a76225a5b749
An Jihyun <hwiyu25@gmail.com>
2021-07-28 15:25:01 +0900

    feat : DocEdit page에서 타겟 문서 찾아 적용하도록 기능 추가

--- src/pages/DocEdit.jsx
+++ src/pages/DocEdit.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import Writer from "../components/Writer";
 
 const DocEdit = () => {
-  // url parameter에서 docId를 받아와서 value에 뿌려주기(리스트에서 찾기)
+  const { docId } = useParams();
 
-  // 더미데이터
-  let docId;
-  const targetDoc = { title: "haha", content: "# ㅎㅎㅎ!" };
-
-  // 진짜 데이터(서버 연결 후 가져올 것)
-  // const targetDoc = useSelector((state) => {
-  //   const idx = state.document.docs.findIndex((doc) => doc.docId === docId);
-  //   return state.document.docs[idx];
-  // });
+  const targetDoc = useSelector((state) => {
+    const idx = state.document.docList.findIndex(
+      (doc) => doc.docId === Number(docId)
+    );
+    return state.document.docList[idx];
+  });
 
   return (
     <>

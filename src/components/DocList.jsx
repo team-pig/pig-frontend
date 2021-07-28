8d8a086b2aa5987b145120a8dd874691b5274e59
An Jihyun <hwiyu25@gmail.com>
2021-07-28 15:28:31 +0900

    feat : 버튼 및 리스트에 페이지간 이동 가능하도록 기능 적용

--- src/components/DocList.jsx
+++ src/components/DocList.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import { Link, useHistory } from "react-router-dom";
+
+// elem
 import { Button } from "../elem";
 
 const DocList = () => {
+  const history = useHistory();
+
   return (
     <div>
       <h4>문서 목록</h4>
       <ul>
-        <li>문서</li>
-        <li>문서</li>
-        <li>문서</li>
+        <Link to="/workspace/1/doc/1">
+          <li>문서</li>
+        </Link>
+        <Link to="/workspace/1/doc/2">
+          <li>문서</li>
+        </Link>
+        <Link to="/workspace/1/doc/3">
+          <li>문서</li>
+        </Link>
       </ul>
-      <Button>페이지 추가</Button>
+      <Button _onClick={() => history.push("/workspace/1/doc/add")}>
+        페이지 추가
+      </Button>
     </div>
   );
 };
--- src/pages/DocView.jsx
+++ src/pages/DocView.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 
 import DocList from "../components/DocList";
 import DocViewer from "../components/DocViewer";
 
 const DocView = (props) => {
   return (
-    <>
+    <Container>
       <DocList />
       <DocViewer />
-    </>
+    </Container>
   );
 };
+
+const Container = styled.section`
+  // 임시 스타일
+  position: relative;
+  z-index: 99;
+`;
 
 export default DocView;

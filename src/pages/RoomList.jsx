c268b6dea21b0d65f155c0039baffd4929af2d7f
slamdunk11 <61729276+slamdunk11@users.noreply.github.com>
2021-07-29 02:49:00 +0900

    feat : RoomList에 getRoomList 연결

--- src/pages/RoomList.jsx
+++ src/pages/RoomList.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 //components
 import Template from "../components/Template";
@@ -8,8 +9,17 @@ import RoomCard from "../components/RoomCard";
 //elements
 import Button from "../elem/Button";
 
+//redux
+import {__getRoomList} from "../redux/modules/room";
+
 const RoomList = () => {
+  const dispatch = useDispatch();
+  const roomList = useSelector((state) => state.room.roomList) || [];
   const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    dispatch(__getRoomList());
+  }, []);
 
   const openModal = () => {
     setShowModal(true);
@@ -24,7 +34,11 @@ const RoomList = () => {
       <Button _onClick={openModal}>방 생성하기</Button>
       <AddRoomModal showModal={showModal} closeModal={closeModal} />
       <div>
-        <RoomCard />
+        {roomList.map((l, idx) => {
+          return(
+            <RoomCard key={idx} {...l} />
+          )
+        })}
       </div>
     </Template>
   );

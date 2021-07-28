d76df702467f0c5c6993f8f44ac1a158c273bedf
slamdunk11 <61729276+slamdunk11@users.noreply.github.com>
2021-07-28 14:29:59 +0900

    feat : roomApi에 deleteRoom 추가

--- src/api/roomApi.js
+++ src/api/roomApi.js
@@ -5,4 +5,5 @@ export const roomApi = {
   getRoomList: (roomList) => instance.get("/rooms", roomList),
   getOneRoom: (roomId) => instance.get(`/rooms/${roomId}/main`, roomId),
   addRoom: (room) => instance.post("/room", room),
+  deleteRoom: (roomId) => instance.delete("/room"),
 };

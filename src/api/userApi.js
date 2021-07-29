3b98ed9e447c87f4d8d2cf40dabe31a63c9ce08d
with-key <rem9238@kakao.com>
2021-07-30 04:20:24 +0900

    feat : register api 추가 구현

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -3,4 +3,5 @@ import { instance } from "./index";
 export const userApi = {
   login: (userInfo) => instance.post("/login", userInfo),
   loginCheck: () => instance.get("/token"),
+  register: (userInfo) => instance.post("register", userInfo),
 };

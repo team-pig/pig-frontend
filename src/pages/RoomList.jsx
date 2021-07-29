d6859312b22402c7e7697939f00a05bc7495372d
slamdunk11 <61729276+slamdunk11@users.noreply.github.com>
2021-07-29 15:46:47 +0900

    feat : JoinModal 연결, RoomCard에 enterRoom 연결

--- src/pages/RoomList.jsx
+++ src/pages/RoomList.jsx
@@ -4,18 +4,21 @@ import { useDispatch, useSelector } from "react-redux";
 //components
 import Template from "../components/Template";
 import AddRoomModal from "../components/AddRoomModal";
+import JoinRoomModal from "../components/JoinRoomModal";
 import RoomCard from "../components/RoomCard";
 
 //elements
 import Button from "../elem/Button";
 
 //redux
-import {__getRoomList} from "../redux/modules/room";
+import { history } from "../redux/configStore";
+import { __getRoomList } from "../redux/modules/room";
 
-const RoomList = () => {
+const RoomList = (props) => {
   const dispatch = useDispatch();
   const roomList = useSelector((state) => state.room.roomList) || [];
   const [showModal, setShowModal] = useState(false);
+  const [isJoin, setIsJoin] = useState(false);
 
   useEffect(() => {
     dispatch(__getRoomList());
@@ -23,21 +26,39 @@ const RoomList = () => {
 
   const openModal = () => {
     setShowModal(true);
+    setIsJoin(false);
+  };
+
+  const openJoinModal = () => {
+    setShowModal(true);
+    setIsJoin(true);
   };
 
   const closeModal = () => {
     setShowModal(false);
   };
+
+  const enterRooom = (roomId) => history.push(`/workspace/${roomId}`);
   return (
     <Template>
       <div>roomlist</div>
+      <Button _onClick={openJoinModal}>코드로 방 참여</Button>
       <Button _onClick={openModal}>방 생성하기</Button>
-      <AddRoomModal showModal={showModal} closeModal={closeModal} />
+      {!isJoin && (
+        <AddRoomModal showModal={showModal} closeModal={closeModal} />
+      )}
+      {isJoin && (
+        <JoinRoomModal showModal={showModal} closeModal={closeModal} />
+      )}
       <div>
-        {roomList.map((l, idx) => {
-          return(
-            <RoomCard key={idx} {...l} />
-          )
+        {roomList.map((room, idx) => {
+          return (
+            <RoomCard
+              key={idx}
+              {...room}
+              onClick={() => enterRooom(room.roomId)}
+            />
+          );
         })}
       </div>
     </Template>

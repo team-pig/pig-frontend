c020e2ab4caca5fa13d4d1e0ad787f42301dcf8e
An Jihyun <hwiyu25@gmail.com>
2021-08-01 02:14:11 +0900

    feat : #35 calendar 월 변경 시마다 전체 일정 불러오는 기능 추가

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
 // component
@@ -7,13 +9,26 @@ import CalendarHeader from "../components/Calendar/CalendarHeader";
 
 // elem
 import { Button } from "../elem";
+import { __loadSchedules } from "../redux/modules/calendar";
 
 const Calendar = (props) => {
+  const { roomId } = useParams();
+
+  const dispatch = useDispatch();
+  const current = useSelector((state) => state.calendar.current);
+
+  // 월이 바뀔 때마다 모든 일정을 가져오도록 설정
+  useEffect(() => {
+    if (current) {
+      dispatch(__loadSchedules(roomId, current.clone().format("YYYYMM")));
+    }
+  }, [current, roomId]);
+
   return (
     <CalendarBox>
+      <Button>일정 추가</Button>
       <CalendarHeader />
       <CalendarBody />
-      <Button>일정 추가</Button>
     </CalendarBox>
   );
 };

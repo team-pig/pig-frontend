4c554690ff4363a1a84f66c201a281b0f36629bc
slamdunk11 <61729276+slamdunk11@users.noreply.github.com>
2021-08-06 01:22:51 +0900

    feat : team-pig #57 - Dropdown 생성

--- src/components/Drop.jsx
+++ src/components/Drop.jsx
@@ -1,20 +1,50 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Text } from "../elem/index";
 
-const Item = ({ children, _onClick, direction, history }) => {
+const Item = ({ children, width, height, _onClick, color, direction }) => {
   return (
-    <Link>
+    <Btn width={width} height={height} color={color} onClick={_onClick}>
       <Text>{children}</Text>
-    </Link>
+    </Btn>
   );
 };
 
-const Container = ({children, size, direction, type, shadow}) => {
+const Container = ({ children, width, height, direction, type, shadow }) => {
   const [isMenuVisible, setIsMenuVisible] = useState("");
-  return(
-    
-  )
-}
+  return (
+    <Wrapper width={width} height={height} shadow={shadow}>
+      {children}
+    </Wrapper>
+  );
+};
 
-const Link = styled.div``;
+const Btn = styled.button`
+  /* display: flex;
+  justify-content: center;
+  align-items: center; */
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+
+  color: ${(props) => `var(--${props.color})`};
+  font-size: 16px;
+  font-weight: bold;
+  line-height: 22px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+
+  border-radius: 5px;
+  box-shadow: 0px 0px 12px rgb(0 0 0 / 10%);
+`;
+
+const Drop = {
+  Container,
+  Item,
+};
+
+export default Drop;

77eade5735948dca37c8c6b8c96fc1f2b389bd79
with-key <rem9238@kakao.com>
2021-08-06 02:45:27 +0900

    feat : #67 card api 추가

--- src/api/cardApi.js
+++ src/api/cardApi.js
@@ -1,10 +1,28 @@
 import { instance } from "./index";
 
 export const cardApi = {
-  getCards: () => instance.get(),
-  getCardById: () => instance.get(),
-  editCardContents: () => instance.patch(),
-  editCardLocation: () => instance.patch(),
-  deleteCard: () => instance.delete(),
-  createCard: () => instance.post(),
+  createCard: (roomId, bucketId, cardTitle) =>
+    instance.post(`/room/${roomId}/card`, { bucketId, cardTitle }),
+
+  getCards: (roomId) => instance.get(`/room/${roomId}`),
+
+  getCardById: (roomId, cardId) =>
+    instance.get(`/room/${roomId}/card/${cardId}`),
+  // editCardContents: (roomId) => instance.patch(`/room/${roomId}/card`, {}),
+
+  editCardLocation: (roomId, cardId, newBucketInfo) =>
+    instance.patch(`/room/${roomId}/cardLocation`, {
+      cardId,
+      sourceBucket: newBucketInfo.sourceBucketId,
+      sourceBucketOrder: newBucketInfo.sourceBucketOrder,
+      destinationBucket: newBucketInfo.destinationBucketId,
+      destinationBucketOrder: newBucketInfo.destinationBucketOrder,
+    }),
+
+  deleteCard: (bucketId, cardId, roomId) =>
+    instance.delete(`/room/${roomId}/card`, {
+      data: {
+        cardId,
+      },
+    }),
 };

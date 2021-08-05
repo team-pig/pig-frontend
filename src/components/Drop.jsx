5d81361e52be88cddc74d88eb1781f3ad7d1cedb
slamdunk11 <61729276+slamdunk11@users.noreply.github.com>
2021-08-06 04:32:38 +0900

    feat : team-pig #57 - Drop view 추가

--- src/components/Drop.jsx
+++ src/components/Drop.jsx
@@ -2,18 +2,46 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { Text } from "../elem/index";
 
-const Item = ({ children, width, height, _onClick, color, direction }) => {
+const Item = ({
+  children,
+  width,
+  height,
+  _onClick,
+  color,
+  direction,
+  ...rest
+}) => {
   return (
-    <Btn width={width} height={height} color={color} onClick={_onClick}>
+    <Btn
+      width={width}
+      height={height}
+      color={color}
+      onClick={_onClick}
+      {...rest}
+    >
       <Text>{children}</Text>
     </Btn>
   );
 };
 
-const Container = ({ children, width, height, direction, type, shadow }) => {
-  const [isMenuVisible, setIsMenuVisible] = useState("");
+const Container = ({
+  children,
+  width,
+  height,
+  display,
+  direction,
+  type,
+  shadow,
+  ...rest
+}) => {
   return (
-    <Wrapper width={width} height={height} shadow={shadow}>
+    <Wrapper
+      display={display}
+      width={width}
+      height={height}
+      shadow={shadow}
+      {...rest}
+    >
       {children}
     </Wrapper>
   );
@@ -23,9 +51,9 @@ const Btn = styled.button`
   /* display: flex;
   justify-content: center;
   align-items: center; */
+
   width: ${(props) => props.width};
   height: ${(props) => props.height};
-
   color: ${(props) => `var(--${props.color})`};
   font-size: 16px;
   font-weight: bold;
@@ -33,13 +61,19 @@ const Btn = styled.button`
 `;
 
 const Wrapper = styled.div`
-  display: flex;
+  position: relative;
+  top: 260px;
+  left: 290px;
+  display: ${(props) => (props.display ? "flex" : "none")};
   flex-direction: column;
+  z-index: var(--indexHeader);
+
   width: ${(props) => props.width};
   height: ${(props) => props.height};
-
+  background-color: var(--white);
   border-radius: 5px;
-  box-shadow: 0px 0px 12px rgb(0 0 0 / 10%);
+  box-shadow: -0.01px -0.01px 2px rgb(0 0 0 / 10%),
+    6px 6px 15px rgba(0 0 0 / 0.1);
 `;
 
 const Drop = {

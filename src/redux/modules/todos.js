4f0ce4e214dbbc103a935fb936044003e8b8102d
with-key <rem9238@kakao.com>
2021-08-06 18:34:18 +0900

    feat : #68 todos crud 서버 연결을 위한 redux module 수정

--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -1,91 +1,108 @@
 import { createAction, handleActions } from "redux-actions";
 import produce from "immer";
+import { todoApi } from "../../api/todoApi";
 
 const LOAD_TODOS = "todos/LOAD_TODOS";
 const CREATE_TODO = "todos/CREATE_TODO";
 const DELETE_TODO = "todos/DELETE_TODO";
-const EDIT_TODO = "todos/EDIT_TODO";
+const EDIT_TODO_TITLE = "todos/EDIT_TODO_TITLE";
+const IS_CHECKED_TODO = "todos/IS_CHECKED_TODO";
 
 /**
  * Action creator
  */
-const laodTodos = createAction(LOAD_TODOS, () => ({}));
-const createTodo = createAction(CREATE_TODO, (cardId, newTodoTitle) => ({
-  cardId,
-  newTodoTitle,
+const laodTodos = createAction(LOAD_TODOS, (todos) => ({ todos }));
+const createTodo = createAction(CREATE_TODO, (newTodo) => ({
+  newTodo,
 }));
 const deleteTodo = createAction(DELETE_TODO, (todoId) => ({ todoId }));
-const editTodo = createAction(EDIT_TODO, (todoId, newTodoTitle) => ({
+const editTodoTitle = createAction(EDIT_TODO_TITLE, (todoId, newTodoTitle) => ({
   todoId,
   newTodoTitle,
+}));
+
+const isCheckedTodo = createAction(IS_CHECKED_TODO, (todoId, isChecked) => ({
+  todoId,
+  isChecked,
 }));
 
 /**
  * Thunk
  */
-export const __loadTodos =
-  () =>
-  async (dispatch, getState, { history }) => {
-    // conat { data } = await
-    // dispatch();
-  };
+export const __loadTodos = (roodId, cardId) => async (dispatch) => {
+  try {
+    const { data } = await todoApi.getTodo(roodId, cardId);
+    dispatch(laodTodos(data.todos));
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-export const __createTodo =
-  (cardId, todoTitle) =>
-  async (dispatch, getState, { history }) => {
-    dispatch(createTodo(cardId, todoTitle));
-  };
-
-export const __editToto =
-  (todoId, newTodoTitle, isChecked) =>
-  async (dispatch, getState, { history }) => {
-    dispatch(editTodo(todoId, newTodoTitle));
-  };
-
-export const __deleteTodo =
-  (todoId) =>
-  async (dispatch, getState, { history }) => {
-    // const {data} = await
-    dispatch(deleteTodo(todoId));
-  };
-
-const initialState = {
-  todos: [
-    {
-      todoId: "todo-1",
-      todoTitle: "모던 자바스크립트 deep dive 책 사기",
+export const __createTodo = (roomId, cardId, todoTitle) => async (dispatch) => {
+  try {
+    const { data } = await todoApi.createTodo(roomId, cardId, todoTitle);
+    const newTodo = {
+      todoId: data.todoId,
+      todoTitle,
       isChecked: false,
       members: [
         {
-          memberId: "user-1",
-          memberName: "예상기",
+          memberId: "",
+          memberName: "",
         },
       ],
-    },
-  ],
+    };
+    dispatch(createTodo(newTodo));
+  } catch (e) {
+    console.log(`투두생성 실패! ${e}`);
+  }
 };
 
-let todoCnt = 2;
+export const __editTotoTitle =
+  (roomId, todoId, todoTitle) => async (dispatch) => {
+    try {
+      await todoApi.editTodoTtitle(roomId, todoId, todoTitle);
+      dispatch(editTodoTitle(todoId, todoTitle));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+export const __checkedTodo =
+  (roomId, todoId, isChecked) => async (dispatch) => {
+    try {
+      await todoApi.checkedTodo(roomId, todoId, isChecked);
+      dispatch(isCheckedTodo(todoId, isChecked));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+export const __deleteTodo = (roomId, todoId) => async (dispatch) => {
+  try {
+    await todoApi.deleteTodo(roomId, todoId);
+    dispatch(deleteTodo(todoId));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const initialState = {
+  todos: [],
+};
+
 export const todos = handleActions(
   {
-    [LOAD_TODOS]: (state, { payload }) => produce(state, (draft) => {}),
+    [LOAD_TODOS]: (state, { payload }) =>
+      produce(state, (draft) => {
+        draft.todos = payload.todos;
+      }),
+
     [CREATE_TODO]: (state, { payload }) =>
       produce(state, (draft) => {
-        const { newTodoTitle } = payload;
-        const newTodo = {
-          todoId: `todo-${todoCnt}`,
-          todoTitle: newTodoTitle,
-          isChecked: false,
-          members: [
-            {
-              memberId: "",
-              memberName: "예상기",
-            },
-          ],
-        };
-        draft.todos.push(newTodo);
-        todoCnt++;
+        draft.todos.push(payload.newTodo);
       }),
+
     [DELETE_TODO]: (state, { payload }) =>
       produce(state, (draft) => {
         const targetIdx = state.todos.findIndex(
@@ -93,7 +110,8 @@ export const todos = handleActions(
         );
         draft.todos.splice(targetIdx, 1);
       }),
-    [EDIT_TODO]: (state, { payload }) =>
+
+    [EDIT_TODO_TITLE]: (state, { payload }) =>
       produce(state, (draft) => {
         const { todoId, newTodoTitle } = payload;
         const targetIdx = state.todos.findIndex(
@@ -101,7 +119,17 @@ export const todos = handleActions(
         );
         draft.todos[targetIdx].todoTitle = newTodoTitle;
       }),
+
+    [IS_CHECKED_TODO]: (state, { payload }) =>
+      produce(state, (draft) => {
+        const { todoId, isChecked } = payload;
+        const targetIdx = state.todos.findIndex(
+          (todo) => todo.todoId === todoId
+        );
+        draft.todos[targetIdx].isChecked = isChecked;
+      }),
   },
+
   initialState
 );
 

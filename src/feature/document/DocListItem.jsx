0ae7744d31285f5a15cf65f45085e1ccc3b78895
An Jihyun <hwiyu25@gmail.com>
2021-08-07 13:22:37 +0900

    feat : document 수정 가능 여부 확인 api 추가

---
+++ src/feature/document/DocListItem.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import styled from "styled-components";
+import { useHistory, useParams } from "react-router-dom";
+
+import Icon from "../../components/Icon";
+
+const DocListItem = ({ doc }) => {
+  const history = useHistory();
+  const { roomId, docId } = useParams();
+
+  const showDocDetail = (docId) =>
+    history.push(`/workspace/${roomId}/doc/${docId}`);
+
+  return (
+    <>
+      <Item onClick={() => showDocDetail(doc.docId)}>
+        <IconBox>
+          <Icon icon="document" size="20px" />
+        </IconBox>
+        <Name>{doc.title}</Name>
+        <IconBtn>
+          <RemoveIcon icon="remove" size="20px" />
+        </IconBtn>
+      </Item>
+    </>
+  );
+};
+
+const Item = styled.li`
+  display: flex;
+  align-items: center;
+  height: 42px;
+  padding: 10px 20px;
+  transition: color 100ms ease-in-out;
+  cursor: pointer;
+
+  &:hover {
+    color: var(--black);
+    background-color: var(--line);
+    font-weight: 600;
+
+    /* ${IconBtn} {
+      display: initial;
+    } */
+  }
+`;
+
+const IconBox = styled.div`
+  flex-shrink: 0;
+  width: 20px;
+  height: 20px;
+`;
+
+const Name = styled.p`
+  width: 100%;
+  margin-left: 10px;
+  color: var(--darkgrey);
+  font-size: 1.6rem;
+  line-height: 2.2rem;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  text-decoration: none;
+`;
+
+const RemoveIcon = styled(Icon)`
+  color: var(--darkgrey);
+  transition: color 230ms ease-in-out;
+`;
+
+const IconBtn = styled.button`
+  display: none;
+  height: 100%;
+  padding: 10px 20px;
+  margin-right: -20px;
+
+  &:hover {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    ${RemoveIcon} {
+      color: var(--notice);
+    }
+  }
+`;
+
+export default DocListItem;

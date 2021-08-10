305d6b267187540731a6b216db06197b3c94270b
with-key <rem9238@kakao.com>
2021-08-11 02:52:42 +0900

    test : room member load 로직 테스트 적용

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Chart from "../feature/board/Chart";
 import Template from "../components/Template";
+import { __loadMembers } from "../redux/modules/member";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router";
 
 const Board = () => {
+  const dispatch = useDispatch();
+  const { roomId } = useParams();
+  console.log(roomId);
+
+  useEffect(() => {
+    dispatch(__loadMembers(roomId));
+  }, []);
+
   return (
     <Template>
       <Chart />

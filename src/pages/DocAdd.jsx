e62375e686e931f989d186087f97246655f47f33
An Jihyun <hwiyu25@gmail.com>
2021-08-11 22:57:11 +0900

    fix : DocAdd 페이지에서 작성 중 페이지를 벗어날 경우 안내메시지 보이도록 수정

--- src/pages/DocAdd.jsx
+++ src/pages/DocAdd.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { Prompt } from "react-router-dom";
 
 // component
 import Writer from "../feature/document/Writer";
 
 const DocAdd = () => {
+  const [showPrompt, setShowPrompt] = useState(true);
+
+  useEffect(() => {
+    return async () => {
+      setShowPrompt(false);
+    };
+  }, []);
+
   return (
-    <Container>
-      <Writer />
-    </Container>
+    <>
+      <Prompt
+        when={showPrompt}
+        message="문서가 저장되지 않았습니다. 정말로 떠나시겠습니까?"
+      />
+      <Container>
+        <Writer />
+      </Container>
+    </>
   );
 };
 

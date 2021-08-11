23a40258a4550de6edb9f03d5d4b0757a78870ca
with-key <rem9238@kakao.com>
2021-08-11 17:01:20 +0900

    style : 카드 디자인 적용

--- src/feature/board/Card.jsx
+++ src/feature/board/Card.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import styled, { css } from "styled-components";
 import { Draggable } from "react-beautiful-dnd";
 import { useParams } from "react-router-dom";
+import moment from "moment";
 
 // elem & compo
 import { Text } from "../../elem";
@@ -9,12 +10,14 @@ import Todos from "./Todos";
 import CardModal from "./CardModal";
 import ModalForms from "./ModalForms";
 import Icon from "../../components/Icon";
+import flex from "../../themes/flex";
 
 // redux & api
 import { useSelector, useDispatch } from "react-redux";
 import { __loadCardById, __deleteCard } from "../../redux/modules/board";
 
 const Card = ({ card, index, bucketId }) => {
+  console.log(card);
   const dispatch = useDispatch();
   const currentContent = useSelector((state) => state.board.card);
   const [showModal, setShowModal] = useState(false);
@@ -32,7 +35,8 @@ const Card = ({ card, index, bucketId }) => {
       <Draggable draggableId={card.cardId} index={index}>
         {(provided, snapshot) => (
           <Container
-            onClick={() => {
+            onClick={(e) => {
+              e.stopPropagation();
               setShowModal(true);
               dispatch(__loadCardById(roomId, card.cardId));
             }}
@@ -41,24 +45,55 @@ const Card = ({ card, index, bucketId }) => {
             ref={provided.innerRef}
             isDragging={snapshot.isDragging}
           >
-            <Flex jc="flex-start" mg="0 0 10px 0">
-              <Text type="body_2">{card.cardTitle}</Text>
-              <Icon
-                icon="remove"
-                size="14px"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  dispatch(__deleteCard(bucketId, card.cardId, roomId));
-                }}
-              />
-            </Flex>
-            <Text type="body_3">{card.desc}</Text>
+            <CardHeader>
+              <CardTitle>
+                <Dot bgColor={card.color} />
+                <Text type="body_2">{card.cardTitle}</Text>
+              </CardTitle>
+              <CardDeleteBtn>
+                <Icon
+                  icon="remove"
+                  size="14px"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    dispatch(__deleteCard(bucketId, card.cardId, roomId));
+                  }}
+                />
+              </CardDeleteBtn>
+            </CardHeader>
+            <CardBody>
+              <Text type="body_3">{card.desc}</Text>
+            </CardBody>
+            <CardFooter>
+              <EndDate>
+                <Text type="body_4" color="grey">
+                  {card.endDate}
+                </Text>
+                <Text type="body_4" color="notice">
+                  D-{moment(moment(card.endDate) - Date.now()).format("DD")}
+                </Text>
+              </EndDate>
+              <CardStat>
+                <StatCnt>
+                  <Icon icon="checkbox" size="20px" />
+                  <Text color="grey" type="body_4">
+                    7
+                  </Text>
+                </StatCnt>
+                <StatCnt>
+                  <Icon icon="my" size="20px" />
+                  <Text color="grey" type="body_4">
+                    14
+                  </Text>
+                </StatCnt>
+              </CardStat>
+            </CardFooter>
           </Container>
         )}
       </Draggable>
       {Object.keys(modalContent).length !== 0 && (
         <CardModal showModal={showModal} setShowModal={setShowModal}>
-          <ModalForms content={modalContent} setContent={setModalContent} />
+          <ModalForms content={modalContent} />
           <TodosHeader type="sub_2" color="black">
             할 일
           </TodosHeader>
@@ -75,27 +110,63 @@ const Container = styled.div`
   background-color: var(--white);
   word-break: break-all;
   width: 280px;
-  min-height: 170px;
   font-size: 1.8rem;
   cursor: pointer !important;
-`;
-
-const Flex = styled.div`
-  display: flex;
-  justify-content: ${(props) => (props.jc ? props.jc : "center")};
-  align-items: ${(props) => (props.ai ? props.ai : "center")};
-  ${(props) =>
-    props.border &&
-    css`
-      border: 1px solid red;
-    `}
-  padding: ${(props) => props.pd};
-  margin: ${(props) => props.mg};
+  /* border: ${(props) => props.isDragging && `1px solid ${props.bgColor}`}; */
 `;
 
 const TodosHeader = styled(Text)`
   padding: 0 40px;
   margin-bottom: 21px;
+`;
+
+const Dot = styled.div`
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background-color: ${(props) => props.theme.colors[props.bgColor]};
+`;
+
+const CardHeader = styled.div`
+  ${flex("between", "center")};
+  margin-bottom: 10px;
+`;
+
+const CardTitle = styled.div`
+  ${flex("between", "center")};
+  gap: 10px;
+`;
+
+const CardDeleteBtn = styled.div`
+  ${flex("center", "center")};
+  transition: 200ms transform ease-in-out;
+  &:hover {
+    transform: scale(1.3);
+  }
+`;
+const CardBody = styled.div`
+  width: 252px;
+  /* height: 24px; */
+  margin-bottom: 20px;
+`;
+const CardFooter = styled.div`
+  ${flex("between", "center")};
+  height: 24px;
+`;
+
+const EndDate = styled.div`
+  ${flex("between", "center")};
+  gap: 10px;
+`;
+
+const CardStat = styled.div`
+  ${flex("between", "center")};
+  gap: 14px;
+`;
+
+const StatCnt = styled.div`
+  ${flex("between", "center")};
+  gap: 6px;
 `;
 
 export default Card;

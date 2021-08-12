66f2f6e4e2ca16d93019ed93673916c87e28e484
with-key <rem9238@kakao.com>
2021-08-12 23:14:54 +0900

    style : [보드] 페이지 레이아웃 수정

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import Chart from "../feature/board/Chart";
-import Template from "../components/Template";
 import { __loadMembers } from "../redux/modules/member";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router";
@@ -8,17 +7,12 @@ import { useParams } from "react-router";
 const Board = () => {
   const dispatch = useDispatch();
   const { roomId } = useParams();
-  console.log(roomId);
 
   useEffect(() => {
     dispatch(__loadMembers(roomId));
   }, []);
 
-  return (
-    <Template>
-      <Chart />
-    </Template>
-  );
+  return <Chart />;
 };
 
 export default Board;

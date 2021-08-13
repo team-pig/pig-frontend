d5fc233d74df7ce24daad3dd704d9fcf921c18cd
with-key <rem9238@kakao.com>
2021-08-14 07:07:38 +0900

    feat : 메인 페이지, dashboard api 구현

--- src/api/dashBoardApi.js
+++ src/api/dashBoardApi.js
@@ -2,4 +2,9 @@ import { instance } from "./index";
 
 export const dashBoardApi = {
   loadAllStatus: (roomId) => instance.get(`/room/${roomId}/main/status`),
+  editMyProfile: (roomId, newMyProfile) =>
+    instance.patch(`/room/${roomId}/myprofile`, { newMyProfile }),
+
+  loadAllRoomInfo: (roomId) => instance.get(`/room/${roomId}/main`),
+  editRoomInfos: (newRoomInfos) => instance.patch("/room", newRoomInfos),
 };

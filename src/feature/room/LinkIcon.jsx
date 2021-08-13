898faadcda380a140ac41798405e4f7f9092bc64
slamdunk11 <61729276+slamdunk11@users.noreply.github.com>
2021-08-13 11:26:21 +0900

    feat : team-pig #170 inviteCode 복사 기능 구현 및 적용

---
+++ src/feature/room/LinkIcon.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "styled-components";
+
+import Icon from "../../components/Icon";
+
+import IconBtn from "../../elem/IconBtn";
+
+const LinkIcon = ({inviteCode}) => {
+  const copyCodeRef = React.useRef();
+
+  const copyCode = (e) => {
+    e.stopPropagation();
+
+    if(!document.queryCommandSupported("copy")){
+      return alert("복사기능 지원되지 않는 브라우저입니다.");
+    }
+    copyCodeRef.current.select();
+    document.execCommand("copy");
+    e.target.focus();
+    console.log("복사완료");
+  }
+  return(
+    <>
+    <Box>
+    <IconBtn padding="0px" _onClick={copyCode}>
+      <Icon icon="link" size="24px"/>
+    </IconBtn>
+    <TextArea
+      ref={copyCodeRef}
+      value={inviteCode}
+      readOnly
+    />
+    </Box>
+    </>
+  )
+};
+
+const TextArea = styled.textarea`
+  
+`;
+
+const Box = styled.div`
+position: relative;
+z-index: 28;
+overflow: hidden;
+width: 24px;
+height: 24px;
+`;
+
+export default LinkIcon;

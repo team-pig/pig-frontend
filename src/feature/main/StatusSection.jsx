0fbda0e1aadf73749adae9516b27812f6bba0972
with-key <rem9238@kakao.com>
2021-08-14 07:12:08 +0900

    feat : 메인 페이지, 팀원현황 기능 구현

--- src/feature/main/StatusSection.jsx
+++ src/feature/main/StatusSection.jsx
@@ -5,52 +5,26 @@ import styled from "styled-components";
 import ProjectStatus from "./ProjectStatus";
 import Members from "./Members";
 
+// redux
+import { useSelector } from "react-redux";
+
 const StatusSection = () => {
+  const { projectStatus, memberStatus } = useSelector(
+    (state) => state.dashBoard
+  );
+
+  console.log(memberStatus);
   //가짜 데이터
   const project = {
     totalTodos: 120,
     completedTodos: 99,
   };
 
-  const members = [
-    {
-      userId: "dsfk231",
-      nickname: "유리",
-      desc: "MBTI안해요. 물어보지마세요.",
-      tags: ["자료검색", "침묵의네이버"],
-      totalTodos: 20,
-      completedTodos: 10,
-    },
-    {
-      userId: "dsf23dsfk",
-      nickname: "짱구",
-      desc: "상태메세지가 들어갑니다.",
-      tags: ["조장", "광대담당", "INFP"],
-      totalTodos: 20,
-      completedTodos: 1,
-    },
-    {
-      userId: "kj123sdf",
-      nickname: "훈이",
-      desc: "상태메세지가 들어갑니다.",
-      tags: ["발표", "물음표살인마", "ENFP"],
-      totalTodos: 30,
-      completedTodos: 15,
-    },
-    {
-      userId: "po2kjf3",
-      nickname: "철수",
-      desc: "상태메세지가 들어갑니다.",
-      tags: ["자료검색", "말안해요"],
-      totalTodos: 50,
-      completedTodos: 49,
-    },
-  ];
-
+  if (memberStatus.length === 0) return <div></div>;
   return (
     <Container>
-      <ProjectStatus project={project} />
-      <Members members={members} />
+      <ProjectStatus projectStatus={projectStatus} />
+      <Members memberStatus={memberStatus} />
     </Container>
   );
 };

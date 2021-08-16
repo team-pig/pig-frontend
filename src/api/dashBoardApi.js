60923cb13436c6e991018dd112836261a221bc71
with-key <rem9238@kakao.com>
2021-08-17 02:11:28 +0900

    edit : api 호출 파라미터 데이터 구조 수정

--- src/api/dashBoardApi.js
+++ src/api/dashBoardApi.js
@@ -2,8 +2,8 @@ import { instance } from "./index";
 
 export const dashBoardApi = {
   loadAllStatus: (roomId) => instance.get(`/room/${roomId}/main/status`),
-  editMyProfile: (roomId, newMyProfile) =>
-    instance.patch(`/room/${roomId}/myprofile`, { newMyProfile }),
+  editMyProfile: (roomId, willReqParams) =>
+    instance.patch(`/room/${roomId}/myprofile`, willReqParams),
 
   loadAllRoomInfo: (roomId) => instance.get(`/room/${roomId}/main`),
   editRoomInfos: (newRoomInfos) => instance.patch("/room", newRoomInfos),

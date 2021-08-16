c698c290592721504ec410956864248893a62386
with-key <rem9238@kakao.com>
2021-08-16 17:33:07 +0900

    feat : 회원가입 시, 프로필 사진 업로드 기능 추가

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useHistory } from "react-router";
 import { useFormik } from "formik";
@@ -16,11 +16,28 @@ import { Button, Input, Text } from "../elem";
 import { useDispatch } from "react-redux";
 import { __register } from "../redux/modules/user";
 import flex from "../themes/flex";
+import ImageModule from "../components/ImageModule";
 
 const Register = () => {
   const history = useHistory();
-
   const dispatch = useDispatch();
+  const [avatar, setAvatar] = useState("");
+
+  /**
+   *
+   * 함수명은 반드시 getImgUrlFromS3 로 만들어주시고,
+   * 첫번째 파라미터에  callback,  두번째 파라미터에 file 선언해서
+   * const result = await callback(file); 이렇게 선언해줘야 합니다.
+   *
+   * 이후 result를 이용하여 원하는 작업을 하면 됩니다.
+   * result는 이미지 url를 반환합니다.
+   */
+
+  const getImgUrlFromS3 = async (callback, file) => {
+    const result = await callback(file);
+    // console.log(result); // 디버깅 할 때 사용하세요.
+    setAvatar(result);
+  };
 
   const formik = useFormik({
     initialValues: {
@@ -28,13 +45,16 @@ const Register = () => {
       nickname: "",
       password: "",
       confirmPassword: "",
+      color: "blue",
     },
 
     validationSchema: Yup.object({
       email: Yup.string()
         .email("올바른 이메일 주소가 아닙니다.")
         .required("빠뜨린 부분이 있네요! 잊지 말고 이메일을 추가하세요."),
-      nickname: Yup.string().required("닉네임을 입력해주세요!"),
+      nickname: Yup.string()
+        .min(3, "닉네임은 3글자 이상 입력해주세요.")
+        .required("닉네임을 입력해주세요!"),
       password: Yup.string()
         .min(6, "비밀번호가 너무 짧네요! 6자 이상 입력하세요.")
         .matches(/[a-zA-Z]/, "더 강력한 비밀번호를 사용하세요.")
@@ -47,7 +67,7 @@ const Register = () => {
     }),
 
     onSubmit: (values, { resetForm }) => {
-      dispatch(__register(values));
+      dispatch(__register({ ...values, avatar }));
       resetForm();
     },
   });
@@ -58,6 +78,24 @@ const Register = () => {
       <Template>
         <Container>
           <Title type="head_3">반가워요!</Title>
+          <Avatar>
+            {/* 
+              props 소개
+
+              getImgUrlFromS3={getImgUrlFromS3} : [필수] 위에서 선언해준 함수를 반드시 props로 넣어주세요.
+              useInitPreview={true} : [선택] 이미지 초기화 기능 사용여부 입니다. 이것을 사용하면 이미지를 올렸을 때 
+              제거 버튼이 활성화되고, preview, file object 를 초기화합니다. (dafualt는 ture 입니다.)
+
+              useSaveAvartar={false} : [선택] 저장 버튼 사용 여부 입니다. 저장 버튼을 사용하면, 이미지를 선택할 때는 base64로 
+              프리뷰를 제공하고, 저장 버튼을 눌렀을 때 S3로 전송하여 url를 반환합니다. (default는 false 입니다.)
+            
+            */}
+            <ImageModule
+              getImgUrlFromS3={getImgUrlFromS3}
+              // useInitPreview={true}
+              // useSaveAvartar={false}
+            />
+          </Avatar>
           <FormContainer onSubmit={formik.handleSubmit}>
             <Input
               isError={formik.touched.email && Boolean(formik.errors.email)}
@@ -125,10 +163,10 @@ const Register = () => {
 };
 
 const Container = styled.section`
+  width: 400px;
+  margin: 0 auto;
   ${flex("start", "center", false)}
-  width: 100%;
   margin-top: 100px;
-
   ${({ theme }) => theme.device.mobile} {
     --minPadding: 18px;
     width: 100%;
@@ -139,7 +177,6 @@ const Container = styled.section`
 
 const FormContainer = styled.form`
   width: 380px;
-
   ${({ theme }) => theme.device.mobile} {
     width: 100%;
     max-width: 380px;
@@ -158,6 +195,11 @@ const Title = styled(Text)`
 
 const RegisterBtn = styled(Button)`
   margin-top: 60px;
+`;
+
+const Avatar = styled.div`
+  ${flex()}
+  margin-bottom : 20px;
 `;
 
 export default Register;

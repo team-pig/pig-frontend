b8fa7e88dc7186dc7345fd87d17f0be259b1406b
An Jihyun <hwiyu25@gmail.com>
2021-08-17 20:52:38 +0900

    feat : 서버로부터 메시지 받는 socket io key 생성

--- src/shared/useSocket.js
+++ src/shared/useSocket.js
@@ -5,18 +5,20 @@ const ENDPOINT = "http://localhost:4000";
 
 export const initiateSocket = () => {
   socket = io(ENDPOINT, { transports: ["websocket"] });
-  console.log(`Connecting socket...`);
-  if (socket) socket.emit("join");
 };
 export const disconnectSocket = () => {
-  console.log("Disconnecting socket...");
   if (socket) socket.disconnect();
 };
 export const subscribeToChat = (cb) => {
-  if (!socket) return console.log("no socket");
+  if (!socket) return;
   socket.on("message", (data) => {
-    console.log("Websocket event received!");
     return cb(null, data);
+  });
+};
+export const getMessages = (cb) => {
+  if (!socket) return;
+  socket.on("messages", (messages) => {
+    return cb(null, messages);
   });
 };
 export const sendMessage = (roomId, nickname, text) => {

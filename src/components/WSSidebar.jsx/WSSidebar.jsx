f3a87169cb96d9f3da113447b9c31077087e5994
An Jihyun <hwiyu25@gmail.com>
2021-08-19 19:51:20 +0900

    feat : 워크스페이스 내 sidebar를 여닫는 기능 추가

--- src/components/WSSidebar.jsx/WSSidebar.jsx
+++ src/components/WSSidebar.jsx/WSSidebar.jsx
@@ -4,14 +4,31 @@ import styled from "styled-components";
 import Notice from "./Notice";
 import RoomMember from "./RoomMember";
 import Chat from "../../feature/chat/Chat";
+import { IconBtn } from "../../elem";
+import Icon from "../Icon";
+import { useDispatch, useSelector } from "react-redux";
+import { showSidebar } from "../../redux/modules/resize";
 
 const WSSidebar = () => {
+  const dispatch = useDispatch();
+
+  const isShowSidebar = useSelector((state) => state.resize.isShowSidebar);
+
+  const handleSidebar = () => dispatch(showSidebar());
+
   return (
-    <Container>
-      <Notice />
-      <RoomMember />
-      <Chat />
-    </Container>
+    <>
+      <Container sidebar={isShowSidebar}>
+        <Notice _onClick={handleSidebar} />
+        <RoomMember />
+        <Chat />
+      </Container>
+      {!isShowSidebar && (
+        <IconBox _onClick={handleSidebar}>
+          <Icon icon="arrow-down" size="20px" color="var(--white)" />
+        </IconBox>
+      )}
+    </>
   );
 };
 
@@ -21,13 +38,26 @@ const Container = styled.aside`
 
   position: fixed;
   top: 48px;
-  right: 0;
+  right: ${(props) => (props.sidebar ? 0 : `-280px;`)};
   flex-shrink: 0;
   flex-grow: 0;
+  z-index: var(--indexSidebar);
   width: 260px;
   height: calc(100vh - var(--minusHeight));
   border-left: 1px solid var(--line);
   background-color: var(--white);
+  transition: right 500ms ease-in-out;
+`;
+
+const IconBox = styled(IconBtn)`
+  position: fixed;
+  top: 50vh;
+  right: 10px;
+  border-radius: 50%;
+  transform: rotate(90deg);
+  z-index: 20;
+  transition: display 500ms ease-in-out;
+  transition-delay: 2s;
 `;
 
 export default WSSidebar;
--- src/redux/modules/resize.js
+++ src/redux/modules/resize.js
@@ -3,15 +3,18 @@ import produce from "immer";
 
 // action type
 const RESIZE_DOCLIST = "resize/RESIZE_DOCLIST";
+const SHOW_SIDEBAR = "resize/SHOW_SIDEBAR";
 
 // action creator;
 export const resizeDocList = createAction(RESIZE_DOCLIST, (width) => ({
   width,
 }));
+export const showSidebar = createAction(SHOW_SIDEBAR);
 
 // initialState
 const initialState = {
   docListWidth: 260,
+  isShowSidebar: false,
 };
 
 // reducer
@@ -20,6 +23,10 @@ const resize = handleActions(
     [RESIZE_DOCLIST]: (state, action) =>
       produce(state, (draft) => {
         draft.docListWidth = action.payload.width;
+      }),
+    [SHOW_SIDEBAR]: (state, action) =>
+      produce(state, (draft) => {
+        draft.isShowSidebar = !state.isShowSidebar;
       }),
   },
   initialState

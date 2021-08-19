3549681ee9a5a6329decb74e9c59d72be532b5a3
with-key <rem9238@kakao.com>
2021-08-19 15:34:58 +0900

    hotfix : auth 로직으로 인해 중복으로 api req를 보내는 증상 임시 주석

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -8,8 +8,6 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import RoomList from "../pages/RoomList";
 import Workspace from "../pages/Workspace";
-import Board from "../pages/Board";
-import Develop from "../pages/Develop";
 
 import Auth from "../shared/auth";
 
@@ -17,11 +15,10 @@ const Router = () => {
   return (
     <Switch>
       <Route path="/" component={Auth(Intro, null)} exact />
-      <Route path="/login" component={Auth(Login, null)} exact />
+      <Route path="/login" component={Auth(Login, false)} exact />
       <Route path="/register" component={Auth(Register, null)} exact />
       <Route path="/roomlist" component={Auth(RoomList, true)} exact />
       <Route path="/workspace/:roomId" component={Auth(Workspace, true)} />
-      <Route path="/develop" component={Auth(Develop, null)} />
       <Redirect from="*" to="/" />
     </Switch>
   );

b9973805a38412f3ae45e6c91ac39b3caf6cd635
An Jihyun <hwiyu25@gmail.com>
2021-08-20 17:39:03 +0900

    fix : 워크스페이스 내에서 이동할 때마다 메세지를 새로 렌더하는 문제 해결
    기존의 roomId를 저장하고 비교해서 같은 roomId이면 redux의 데이터를 갈아끼우지 않도록 설정

--- src/pages/Workspace.jsx
+++ src/pages/Workspace.jsx
@@ -10,8 +10,12 @@ import SEO from "../components/SEO";
 
 // redux & api
 import { __getDocs } from "../redux/modules/document";
-import { __getOneRoom, initRoom } from "../redux/modules/room";
-import { loadMessages, resetMessages } from "../redux/modules/chat";
+import { __getOneRoom } from "../redux/modules/room";
+import {
+  loadMessages,
+  resetMessages,
+  setPrevRoomId,
+} from "../redux/modules/chat";
 
 // socket
 import { joinRoom, leaveRoom, getMessages } from "../shared/useSocket";
@@ -19,10 +23,13 @@ import { joinRoom, leaveRoom, getMessages } from "../shared/useSocket";
 const Workspace = (props) => {
   let { path, url } = useRouteMatch();
   const { roomId } = useParams();
+
   const dispatch = useDispatch();
+
   const { userId, nickname } = useSelector((state) => state.user.user);
   const docs = useSelector((state) => state.document.docList);
   const room = useSelector((state) => state.room.currentRoom);
+  const prevRoomId = useSelector((state) => state.chat.prevLoadRoomId);
 
   useEffect(() => {
     (docs.length === 0 || (docs.length !== 0 && docs[0].roomId !== roomId)) &&
@@ -30,32 +37,33 @@ const Workspace = (props) => {
     dispatch(__getOneRoom(roomId));
   }, [dispatch, roomId]);
 
-  // workspace를 나갈 때마다 store의 message가 reset되도록 함
-  useEffect(() => {
-    return () => {
-      dispatch(resetMessages());
-    };
-  }, [dispatch]);
-
   // workspace에 들어갈 때마다 room에 join, 해당 방의 기존 메세지 받아오는 handler
   useEffect(() => {
-    if (roomId && nickname && userId) {
-      joinRoom(roomId, nickname, userId);
+    if (roomId && nickname && userId && room && room.roomName) {
+      joinRoom(roomId, nickname, userId, room.roomName);
     }
+  }, [dispatch, roomId, nickname, userId, room]);
 
+  useEffect(() => {
     getMessages((err, data) => {
-      if (err) console.log(err);
-      dispatch(loadMessages(data));
+      if (err) {
+        console.log(err);
+        return;
+      }
+      if (prevRoomId !== roomId) {
+        dispatch(setPrevRoomId(roomId));
+        dispatch(loadMessages(data));
+        return;
+      }
     });
-  }, [dispatch, roomId, nickname, userId]);
+  }, [prevRoomId, roomId, dispatch]);
 
   // workspace에서 나갈 때 room에서 leave
   useEffect(() => {
     return () => {
       leaveRoom(roomId, nickname, userId);
-      dispatch(initRoom());
     };
-  }, [roomId, nickname, userId]);
+  }, [roomId, nickname, userId, dispatch]);
 
   return (
     <>
@@ -64,7 +72,7 @@ const Workspace = (props) => {
         <WSHeader url={url} />
         <WSTemplate>
           <WSRouter path={path} />
-          {/* <WSSidebar /> */}
+          <WSSidebar />
         </WSTemplate>
       </>
     </>
--- src/redux/modules/chat.js
+++ src/redux/modules/chat.js
@@ -5,6 +5,7 @@ import produce from "immer";
 const LOAD_MESSAGES = "chat/LOAD_MESSAGES";
 const ADD_MESSAGE = "chat/ADD_MESSAGES";
 const RESET_MESSAGES = "chat/RESET_MESSAGES";
+const SET_PREV_ROOMID = "chat/SET_PREVROOM_ID";
 
 // action creator
 export const loadMessages = createAction(LOAD_MESSAGES, (messages) => ({
@@ -14,10 +15,14 @@ export const addMessage = createAction(ADD_MESSAGE, (message) => ({
   message,
 }));
 export const resetMessages = createAction(RESET_MESSAGES);
+export const setPrevRoomId = createAction(SET_PREV_ROOMID, (newId) => ({
+  newId,
+}));
 
 // initialState
 const initialState = {
   messages: [],
+  prevLoadRoomId: null,
 };
 
 // reducer
@@ -26,7 +31,7 @@ const chat = handleActions(
     [LOAD_MESSAGES]: (state, action) =>
       produce(state, (draft) => {
         console.log(state);
-        draft.messages = [...action.payload.messages, ...state.messages];
+        draft.messages = action.payload.messages;
       }),
     [ADD_MESSAGE]: (state, action) =>
       produce(state, (draft) => {
@@ -35,6 +40,10 @@ const chat = handleActions(
     [RESET_MESSAGES]: (state, action) =>
       produce(state, (draft) => {
         draft.messages = [];
+      }),
+    [SET_PREV_ROOMID]: (state, action) =>
+      produce(state, (draft) => {
+        draft.prevLoadRoomId = action.payload.newId;
       }),
   },
   initialState

ea07fb0fcc6b8d57bd65f83746823f92a4cb25c5
with-key <rem9238@kakao.com>
2021-08-21 14:50:55 +0900

    test : auth hoc 임시 제거

--- src/shared/auth.js
+++ src/shared/auth.js
@@ -8,31 +8,26 @@ export default (SpecialComponent, option, adminRoute = null) => {
   const dispatch = useDispatch();
   const AuthenticateCheck = ({ history, match, location }) => {
     useEffect(() => {
-      // 권한 설정 필요 없는 경우 return
       if (option === null) return;
-
       const loginCheck = async () => {
         try {
-          // login checking
           const {
             data: { ok: isLogin, user },
           } = await userApi.loginCheck();
-          // 로그인 한 사용자 login, register 페이지 접근 차단
-          if (
-            isLogin === true &&
-            option === false &&
-            (match.path === "/login" || match.path === "/register")
-          ) {
-            // history.replace("/roomlist");
-          }
-          // 리덕스 업데이트
+
+          // if (
+          //   isLogin === true &&
+          //   option === false &&
+          //   (match.path === "/login" || match.path === "/register")
+          // ) {
+          // }
+
           dispatch(__loginCheck(isLogin, user));
         } catch (e) {
-          // login, register 페이지로 가는 것은 예외처리
-          if (match.path === "/login" || match.path === "/register") return;
+          // if (match.path === "/login" || match.path === "/register") return;
 
           // 위 2개를 제외한 모든 url에 login 하지 않은 사용자가 접근하면 login 페이지로
-          if (!!e.response.data) history.replace("/login");
+          // if (!!e.response.data) history.replace("/login");
           dispatch(__loginCheck(false));
         }
       };

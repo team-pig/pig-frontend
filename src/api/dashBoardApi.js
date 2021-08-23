cbe8a0030e5aac5fe84ca44c20b1a91ef89edd70
with-key <rem9238@kakao.com>
2021-08-24 02:48:47 +0900

    edit : 불필요한 리덕스 모듈 및 api instance 제거 (dashboard)

--- src/api/dashBoardApi.js
+++
@@ -1,9 +0,0 @@
-import { instance } from "./index";
-
-export const dashBoardApi = {
-  loadAllStatus: (roomId) => instance.get(`/room/${roomId}/main/status`),
-  editMyProfile: (roomId, willReqParams) =>
-    instance.patch(`/room/${roomId}/myprofile`, willReqParams),
-
-  editRoomInfos: (newRoomInfos) => instance.patch("/room", newRoomInfos),
-};

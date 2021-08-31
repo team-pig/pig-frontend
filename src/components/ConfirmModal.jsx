e900d69c355318b5a1794828bfe7450390fcacd9
slamdunk11 <61729276+slamdunk11@users.noreply.github.com>
2021-08-31 16:03:10 +0900

    feat : team-pig #96 - ConfirmModal 반응형 뷰 적용

--- src/components/ConfirmModal.jsx
+++ src/components/ConfirmModal.jsx
@@ -12,23 +12,23 @@ const ConfirmModal = ({ msg }) => {
 
   return (
     <>
-    
       <ModalContainer>
         <ModalOverlay onClick={onCancel}>
           <ModalContent>
             <Desc>{msg}</Desc>
             <BtnSet>
-              <Button size="150" _onClick={onConfirm}>
-                확인
-              </Button>
-              <Button size="150" _onClick={onCancel}>
-                취소
-              </Button>
+              <LeftBtn>
+                <Button _onClick={onConfirm}>확인</Button>
+              </LeftBtn>
+              <RightBtn>
+                <Button shape="green-outline" _onClick={onCancel}>
+                  취소
+                </Button>
+              </RightBtn>
             </BtnSet>
           </ModalContent>
         </ModalOverlay>
       </ModalContainer>
-    
     </>
   );
 };
@@ -70,12 +70,29 @@ const ModalContent = styled.div`
   width: 404px;
   height: 200px;
   background-color: var(--white);
-  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.03), 0 6px 6px rgba(0, 0, 0, 0.02);
+  ${({ theme }) => theme.device.mobile} {
+    width: 280px;
+  }
 `;
 
 const BtnSet = styled.div`
   ${flex()}
+  width: 300px;
   margin: 24px 0 40px 0;
+  ${({ theme }) => theme.device.mobile} {
+    min-width: 200px;
+    padding: 0 40px 0 40px;
+  }
+`;
+
+const LeftBtn = styled.div`
+  width: 100%;
+`;
+
+const RightBtn = styled.div`
+  width: 100%;
+  margin-left: -1px;
 `;
 
 export default ConfirmModal;

37e72a216a450c19eab63cac81495691039a4326
An Jihyun <hwiyu25@gmail.com>
2021-09-02 22:28:29 +0900

    fix : tutorial skip 시 제대로 작동하지 않는 문제 수정

--- src/feature/tutorial/JoyrideContainer.jsx
+++ src/feature/tutorial/JoyrideContainer.jsx
@@ -3,26 +3,29 @@ import Joyride from "react-joyride";
 import { useDispatch } from "react-redux";
 import { __modifyTutorialStatus } from "../../redux/modules/user";
 
+// 튜토리얼 컴포넌트
 const JoyrideContainer = ({ run, steps, page }) => {
   const dispatch = useDispatch();
 
+  // status가 "finished"일 때 (모든 튜토리얼을 봤거나 중간에 취소하거나 Skip한 경우) 서버에게 해당 유저의 튜토리얼 상태 변경 요청
   const handleJoyrideCallback = useCallback(
     (data) => {
       const { status } = data;
 
-      if (status === "finished") {
+      if (status === "finished" || status === "skipped") {
         dispatch(__modifyTutorialStatus(page));
       }
     },
     [dispatch, page]
   );
 
+  // 스타일 옵션
   const defaultOptions = {
     arrowColor: "#fff",
     backgroundColor: "#fff",
     beaconSize: 36,
     overlayColor: "rgba(0, 0, 0, 0.5)",
-    primaryColor: "#f04",
+    primaryColor: " #4D6C56",
     spotlightShadow: "0 0 15px rgba(0, 0, 0, 0.5)",
     textColor: "#333",
     width: undefined,
